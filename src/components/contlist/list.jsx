9dcfd47eb53817476b8064397858575dcc970640
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-07-07 14:25:19 +0300

    Update list.jsx

--- src/components/contlist/list.jsx
+++ src/components/contlist/list.jsx
@@ -1,22 +1,19 @@
+
 import React from "react";
 import { useDispatch } from "react-redux";
 import lcss from "../contlist/list.module.css";
 import { deleteUser } from "../redux/contactsSlice";
 
-const ContactList = ({ contacts, filter }) => {
+const ContactList = ({contacts, filter}) => {
   const dispatch = useDispatch();
 
   const handleDeleteContact = (contactId) => {
     dispatch(deleteUser(contactId));
   };
 
-  const filteredContacts = contacts.filter((contact) => {
-    const contactName = contact.name || ""; // Handle cases where contact.name is undefined
-    return (
-      typeof contactName === "string" &&
-      contactName.toLowerCase().includes(filter.toLowerCase())
-    );
-  });
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul className={lcss.llist}>
@@ -37,40 +34,7 @@ const ContactList = ({ contacts, filter }) => {
 
 export default ContactList;
 
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import lcss from "../contlist/list.module.css";
-// import { deleteUser } from "../redux/contactsSlice";
 
-// const ContactList = ({ contacts, filter }) => {
-//   const dispatch = useDispatch();
-
-//   const handleDeleteContact = (contactId) => {
-//     dispatch(deleteUser(contactId));
-//   };
-
-//   const filteredContacts = contacts.filter((contact) =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-
-//   return (
-//     <ul className={lcss.llist}>
-//       {filteredContacts.map((contact) => (
-//         <li className={lcss.item} key={contact.id}>
-//           {contact.name}: {contact.number}
-//           <button
-//             className={lcss.bbtn}
-//             onClick={() => handleDeleteContact(contact.id)}
-//           >
-//             delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
 
 // import React from "react";
 // //import { nanoid } from "nanoid";
