6fd6a1a0b0567fb0154440fa37987afd141138ba
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-07-07 14:07:28 +0300

    Update list.jsx

--- src/components/contlist/list.jsx
+++ src/components/contlist/list.jsx
@@ -10,9 +10,13 @@ const ContactList = ({ contacts, filter }) => {
     dispatch(deleteUser(contactId));
   };
 
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = contacts.filter((contact) => {
+    const contactName = contact.name || ""; // Handle cases where contact.name is undefined
+    return (
+      typeof contactName === "string" &&
+      contactName.toLowerCase().includes(filter.toLowerCase())
+    );
+  });
 
   return (
     <ul className={lcss.llist}>
@@ -32,6 +36,41 @@ const ContactList = ({ contacts, filter }) => {
 };
 
 export default ContactList;
+
+// import React from "react";
+// import { useDispatch } from "react-redux";
+// import lcss from "../contlist/list.module.css";
+// import { deleteUser } from "../redux/contactsSlice";
+
+// const ContactList = ({ contacts, filter }) => {
+//   const dispatch = useDispatch();
+
+//   const handleDeleteContact = (contactId) => {
+//     dispatch(deleteUser(contactId));
+//   };
+
+//   const filteredContacts = contacts.filter((contact) =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+
+//   return (
+//     <ul className={lcss.llist}>
+//       {filteredContacts.map((contact) => (
+//         <li className={lcss.item} key={contact.id}>
+//           {contact.name}: {contact.number}
+//           <button
+//             className={lcss.bbtn}
+//             onClick={() => handleDeleteContact(contact.id)}
+//           >
+//             delete
+//           </button>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default ContactList;
 
 // import React from "react";
 // //import { nanoid } from "nanoid";
